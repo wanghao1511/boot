c201d7d297bd9938f00c34f40d1f7ede1e2bb8d7
Wh <2975219753@qq.com>
2019-08-31 11:19:45 +0800

    44444

---
+++ src/main/java/com/example/demo01/com/example/demo01/controller/TestConteoller.java
@@ -0,0 +1,10 @@
+package com.example.demo01.com.example.demo01.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestConteoller {
+    /**
+     * fdasd
+     */
+}
